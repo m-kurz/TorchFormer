Implemented main encoder block and masking for decoder self-attention. Fixed bug in computation of attention scores in multi-head attention. Decoder still needs Encoder/Decoder attention.

test/test_layers.py
@@ -3,13 +3,13 @@ import torch
 import torchformer
 
 
-class TestTransformerBlock(unittest.TestCase):
+class TestTransformerEncoderBlock(unittest.TestCase):
 
     def test_forward(self):
         d_model = 512
         BATCH_SIZE = 10
 
-        model = torchformer.TransformerBlock(d_model=d_model)
+        model = torchformer.TransformerEncoderBlock(d_model=d_model)
         data = torch.randn(BATCH_SIZE, d_model)
         output = model(data)
         self.assertEqual(output.shape, (BATCH_SIZE, d_model))

torchformer/layers.py
@@ -47,44 +47,51 @@ class FullyConnectedLayer(nn.Module):
 
 
 class DotProductAttention(nn.Module):
-    def __init__(self, d_model, d_k, d_v):
+    def __init__(self, d_model, d_k, d_v, mask=False):
         super().__init__()
+        self.mask = mask
+        self.d_k = d_k
         # create weight metrices for query, key and value
-        self.w_query = nn.Parameter(torch.randn(d_model, d_k))
-        self.w_key = nn.Parameter(torch.randn(d_model, d_k))
-        self.w_value = nn.Parameter(torch.randn(d_model, d_v))
+        self.w_q = nn.Parameter(torch.randn(d_model, d_k))
+        self.w_k = nn.Parameter(torch.randn(d_model, d_k))
+        self.w_v = nn.Parameter(torch.randn(d_model, d_v))
 
     def forward(self, value_in):
-        # First project input values to query, key and value
-        value, query, key = self._projection(value_in)
+        # First project input linearly to query, key and value
+        V, Q, K = self._projection(value_in)
         # Calculate attention scores
-        print(value.shape, query.shape, key.shape)
-        scores = torch.tensordot(query, key, dims=([-1], [-1]))
-        scores *= 1. / key.size(-1) ** 0.5
+        scores = torch.matmul(Q, K.transpose(-2, -1)) / self.d_k ** 0.5
+        # Masking
+        if self.mask:
+            mask = torch.tril(torch.ones_like(scores))
+            scores = scores.masked_fill(mask == 0, -float('inf'))
         # Probabilities via Softmax
-        attention = torch.softmax(scores, dim=-1)
-        return torch.matmul(attention, value), attention
+        attention = torch.softmax(scores, dim=-2)
+        return torch.matmul(attention, V), attention
 
     def _projection(self, value_in):
         """Perform projection with query, key and value weights"""
-        query = torch.matmul(value_in, self.w_query)
-        key = torch.matmul(value_in, self.w_key)
-        value = torch.matmul(value_in, self.w_value)
-        return value, query, key
+        v = torch.matmul(value_in, self.w_v)  # (batch_size, seq_len, d_v)
+        q = torch.matmul(value_in, self.w_q)  # (batch_size, seq_len, d_k)
+        k = torch.matmul(value_in, self.w_k)  # (batch_size, seq_len, d_k)
+        return v, q, k
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, d_model, num_heads=1, d_k=None, d_v=None):
+    def __init__(self, d_model, num_heads=1, d_k=None, d_v=None, mask=False):
         super().__init__()
+
         # Set default values
         self.num_heads = num_heads
         if d_k is None:
             d_k = d_model // self.num_heads
         if d_v is None:
             d_v = d_model // self.num_heads
+
         # Create multiple attention heads
         for i in range(num_heads):
-            self.add_module(f'att_{i}', DotProductAttention(d_model, d_k, d_v))
+            self.add_module(f'att_{i}', DotProductAttention(d_model, d_k, d_v, mask))
+
         # Add final projection layer
         self.w_proj = nn.Parameter(torch.randn(self.num_heads * d_v, d_model))
 
@@ -96,38 +103,102 @@ class MultiHeadAttention(nn.Module):
         return torch.matmul(torch.cat(head_outputs, dim=-1), self.w_proj)
 
 
-class TransformerBlock(nn.Module):
+class TransformerEncoderBlock(nn.Module):
     def __init__(self,
                  d_model=512,
                  num_heads=8,
                  d_k=None,
                  d_v=None,
-                 d_ff=None,
+                 d_ff=2048,
                  dropout=0.1
                 ):
         super().__init__()
+
+        # Set default values
         if d_k is None:
             d_k = d_model // num_heads
         if d_v is None:
             d_v = d_model // num_heads
-        if d_ff is None:
-            d_ff = 4 * d_model
+
+        # Create sub-layers
         self.attention = MultiHeadAttention(d_model, num_heads, d_k, d_v)
         self.norm1 = nn.LayerNorm(d_model)
         self.ff = FullyConnectedLayer(d_model, d_ff, activation=nn.ReLU())
         self.norm2 = nn.LayerNorm(d_model)
         if dropout > 0:
-            self.dropout = nn.Dropout(dropout)
+            self.dropout1 = nn.Dropout(dropout)
+            self.dropout2 = nn.Dropout(dropout)
         else:
-            self.dropout = None
+            self.dropout1 = None
+            self.dropout2 = None
 
     def forward(self, x):
+        # First block: Self Attention
         x1 = self.attention(x)
-        if self.dropout is not None:
+        if self.dropout1 is not None:
+            x1 = self.dropout1(x1)
+        x  = self.norm1(x + x1)
+
+        # Second block: Feed-Forward Network
+        x1 = self.ff(x)
+        if self.dropout2 is not None:
+            x1 = self.dropout2(x1)
+        x = self.norm2(x + x1)
+
+        return x
+
+
+class TransformerDecoderBlock(nn.Module):
+    def __init__(self,
+                 d_model=512,
+                 num_heads=8,
+                 d_k=None,
+                 d_v=None,
+                 d_ff=2048,
+                 dropout=0.1
+                ):
+        super().__init__()
+
+        # Set default values
+        if d_k is None:
+            d_k = d_model // num_heads
+        if d_v is None:
+            d_v = d_model // num_heads
+
+        # Create sub-layers
+        self.masked_attention = MultiHeadAttention(d_model, num_heads, d_k, d_v, mask=True)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.attention = MultiHeadAttention(d_model, num_heads, d_k, d_v)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.ff = FullyConnectedLayer(d_model, d_ff, activation=nn.ReLU())
+        self.norm3 = nn.LayerNorm(d_model)
+        if dropout > 0:
+            self.dropout1 = nn.Dropout(dropout)
+            self.dropout2 = nn.Dropout(dropout)
+            self.dropout3 = nn.Dropout(dropout)
+        else:
+            self.dropout1 = None
+            self.dropout2 = None
+            self.dropout3 = None
+
+    def forward(self, x, x_encoder):
+        # First block: Masked Self-Attention
+        x1 = self.masked_attention(x)
+        if self.dropout1 is not None:
             x1 = self.dropout(x1)
         x  = self.norm1(x + x1)
-        x1 = self.ff(x)
-        if self.dropout is not None:
+
+        # Second block: Encoder/Decoder-Attention
+        # TODO: Requires encoder input
+        x1 = self.attention(x)
+        if self.dropout2 is not None:
             x1 = self.dropout(x1)
         x = self.norm2(x + x1)
+
+        # Third block: Feed-Forward Network
+        x1 = self.ff(x)
+        if self.dropout2 is not None:
+            x1 = self.dropout(x1)
+        x = self.norm2(x + x1)
+
         return x